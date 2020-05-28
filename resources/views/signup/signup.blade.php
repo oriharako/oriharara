13b96e99d998f698a0f2fdbdb8407101cc68825a
root <root@os3-366-16226.vs.sakura.ne.jp>
2020-05-28 10:15:44 +0900

    add new file

---
+++ resources/views/signup/signup.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>signin</title>
+    <link rel="stylesheet" href="{{ asset('css/signup/signup.css') }}">
+</head>
+<body>
+
+
+<div class="login">
+  <div class="heading">
+    <h2>Sign up</h2>
+    <form action="#">
+
+      <div class="input-group input-group-lg">
+        <span class="input-group-addon"><i class="fa fa-user"></i></span>
+        <input type="text" class="form-control" placeholder="Username or email">
+          </div>
+
+        <div class="input-group input-group-lg">
+          <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+          <input type="password" class="form-control" placeholder="Password">
+        </div>
+
+        <button type="submit" class="float">Login</button>
+       </form>
+                </div>
+ </div>
+</body>
+</html>
+
